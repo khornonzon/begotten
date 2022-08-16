5b0db6bfabd236809d76df9145e607af701d86d5
khornonzon <t.pasm@mail.ru>
2022-08-16 16:52:14 +0300

    NA PENEK SEL DOLZHEN BIL CREDENTIALS OTDAT

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,12 +1,22 @@
 import { Request, Response, Router } from "express";
 import { AppDataSource } from "../db/data-source";
 import { User } from "../db/entity/User";
-import { CannotAttachTreeChildrenEntityError, FindManyOptions, Repository } from "typeorm";
+import { CannotAttachTreeChildrenEntityError, FindManyOptions, FindOneOptions, Repository } from "typeorm";
 import { timeStamp } from "console";
+
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const tokenKey = '1a2b-3c4d-5e6f-7g8h'
 
 interface queryParams{
   limit?: number,
   offset?: number
+}
+
+interface bodyRegUser{
+  name: string,
+  surname: string,
+  password: string
 }
 export class UserController {
   private static userRepository: Repository<User> =
@@ -28,6 +38,59 @@ export class UserController {
       console.log(err);
       res.sendStatus(500);
     }
+
+  }
+
+  public static async regUser(req: Request, res: Response){
+    try {
+      const {name, surname, password} = req.body;
+      if (!(name&&surname&&password))
+      {
+        res.status(404).send("we need more power.");
+      }
+      const existingUser = await UserController.userRepository.findOne(surname);
+      if (existingUser) {
+        return res.status(409).send("user already exist");
+      }
+      const encryptedPassword = await bcrypt.hash(password, 10);
+      const user = new User();
+      user.name = name;
+      user.surname = surname;
+      user.password = encryptedPassword;
+      const token = jwt.sign({ user_id: user.id}, tokenKey);
+      user.token = token;
+      await UserController.userRepository.save(user);
+      res.status(201).json(user);
+    } catch(err){
+      console.log(err);
+      res.sendStatus(500);
+    }
+  }
+  
+  public static async loginUser(req: Request, res: Response){
+    try {
+      const {surname, password} = req.body;
+      if (!(surname && password)) {
+        res.status(400).send("All input is required");
+      }
+      const user = await UserController.userRepository.findOne(surname);
+  
+      if (user && (await bcrypt.compare(password, user.password))) {
+        const token = jwt.sign(
+          { user_id: user.id },
+          tokenKey
+        );
+        user.token = token;
+        res.status(200).json(user);
+      }
+      res.status(400).send("Invalid Credentials");
+    } catch (err) {
+      console.log(err);
+    }
+    // Our register logic ends here
+  };
+  
+  // ...
 
   }
 
